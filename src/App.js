075594c8994a951ad7d8e49b54b979fc8952af86
sxkzxqw <marsel.vagizov@bk.ru>
2023-02-21 17:39:28 +0300

    added modal

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import React, { useState } from 'react';
-import PostItem from './components/PostItem';
+import React, { useState, useMemo } from 'react';
 import './components/styles/app.css'
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
-import { MyButton } from './components/UI/button/MyButton';
-import { MyInput } from './components/UI/input/MyInput';
-import MySelect from './components/UI/select/MySelect';
+import PostFilter from './components/PostFilter';
+import MyModal from './components/UI/MyModal/MyModal';
+import MyButton from './components/UI/button/MyButton';
 
 function App() {
     const [posts, setPosts] = useState ([
-      {id: 1, title: 'JavaScript', body: 'Description'},
-      {id: 2, title: 'JavaScript 2', body: 'Description 2'},
-      {id: 3, title: 'JavaScript 3', body: 'Description 3'}
+      {id: 1, title: 'zz', body: 'yy'},
+      {id: 2, title: 'gg', body: 'zz'},
+      {id: 3, title: 'dd', body: 'xx'}
     ])
+
+    const [filter, setFilter] = useState({sort: '', query: ''})
+    const [modal, setModal] = useState(false);
+
+
+    const sortedPosts = useMemo(() => {
+      if(filter.sort) {
+        return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
+      }
+      return posts;
+    }, [filter.sort, posts])
+
+    const sortedAndSearchedPosts = useMemo(() => {
+      return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
+    }, [filter.query, sortedPosts])
 
     const createPost = (newPost) => {
       setPosts([...posts, newPost])
+      setModal(false)
     }
 
     const removePost = (post) => {
@@ -24,22 +39,18 @@ function App() {
 
     return (
       <div className="App">
-        <PostForm create={createPost}/>
+        <MyButton onClick={() => setModal(true)}>
+          Создать пользователя
+        </MyButton>
+        <MyModal visible={modal} setVisible={setModal}>
+          <PostForm create={createPost}/>
+        </MyModal>
         <hr style={{margin: '15px 0'}}/>
-        <div>
-            <MySelect
-            defaultValue='Сортировка:'
-            options={[
-              {value: 'title', name: 'По названию'},
-              {value: 'body', name: 'По описанию'}
-          ]}
-
-            />
-        </div>
-        {posts.length
-          ? <PostList remove={removePost} posts={posts} title='Посты про JS'/>
-          : <h1 style={{textAlign: 'center', fontSize: '60px'}}>Постов нет</h1>
-        }
+        <PostFilter 
+        filter={filter} 
+        setFilter={setFilter}
+        />
+         <PostList remove={removePost} posts={sortedAndSearchedPosts} title='Посты про JS'/>
       </div>
     )
 }
--- src/components/UI/select/MySelect.jsx
+++ src/components/UI/select/MySelect.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
-const MySelect = ({options, defaultValue}) => {
+const MySelect = ({options, defaultValue, value, onChange}) => {
     return (
-        <select>
+        <select
+            value={value}
+            onChange={event => onChange(event.target.value)}
+        >
             <option disabled value="">{defaultValue}</option>
-            {options.map(option => 
+            {options.map(option =>
                 <option key={option.value} value={option.value}>
                     {option.name}
                 </option>
-                )}
+            )}
         </select>
     );
 };
